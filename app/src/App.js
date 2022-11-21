9523710d08c6499db846a4d4cc3915ed19f30b66
Desmond Dleema <desmonddleema211@gmail.com>
2022-11-21 22:00:02 +0530

    Sidebar

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ import { TooltipComponent} from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 
 //PAGES
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
 
 import './App.css';
 
