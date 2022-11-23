9dcc6cd4c6f6da84245849fdbba831e1c6b5944e
Desmond Dleema <desmonddleema211@gmail.com>
2022-11-23 18:25:23 +0530

    Employees Page

--- app/src/pages/Employees.jsx
+++ app/src/pages/Employees.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from "@syncfusion/ej2-react-grids";
 
-const Employees = () => {
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Orders = () => {
   return (
-    <div>Employees</div>
+    <div className="mt-24 md:m-10 m-2 md:p-10 p-2 rounded-3xl bg-white dark:bg-secondary-dark-bg">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index)=> (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
-export default Employees
+export default Orders;
+
