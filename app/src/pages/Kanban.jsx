b39f9f3a84a798b6b0d970b9e330fa266abf082d
Desmond Dleema <desmonddleema211@gmail.com>
2022-11-28 14:05:44 +0530

    Kanban - - APP

--- app/src/pages/Kanban.jsx
+++ app/src/pages/Kanban.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
 
-const Kanban = () => {
+import { kanbanData, kanbanGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const  Kanban = () => {
   return (
-    <div>Kanban</div>
+    <div className='m-2 p-2 md:m-10 md:p-10 mt-24 bg-white rounded-3xl'>
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        cardSettings={{ contentField:'Summary', headerField:'Id' }}
+        keyField= "Status"
+      >
+        <ColumnsDirective>
+          { kanbanGrid.map((item, index)=> (
+            <ColumnDirective key={index} {...item} />
+          )) }
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }
 
