a24801c38a01e3a6db995c12f7837c534a838f11
Desmond Dleema <desmonddleema211@gmail.com>
2022-12-01 16:10:12 +0530

    Theme mode

--- app/src/App.js
+++ app/src/App.js
@@ -13,10 +13,10 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode==='dark'? 'dark' : ''}>
         <BrowserRouter> 
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{zIndex: 1000}}>
