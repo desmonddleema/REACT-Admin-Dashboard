9c30cca50fcce9d597d4053bb89861a938f580c5
Desmond Dleema <desmonddleema211@gmail.com>
2022-12-01 16:28:29 +0530

    LineChart - - Chart

--- app/src/components/ThemeSettings.jsx
+++ app/src/components/ThemeSettings.jsx
@@ -12,7 +12,7 @@ const ThemeSettings = () => {
 
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen w-400 dark:text-gray-200 bg-white dark:[#484B52]">
+      <div className="float-right h-screen w-400 dark:text-gray-200 bg-white dark:bg-[#484B52]">
         <div className="flex justify-between items-center ml-4 p-4">
           <p className="font-semibold text-xl">Settings</p>
           <button
--- app/src/pages/Charts/Line.jsx
+++ app/src/pages/Charts/Line.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <div className="mt-24 m-4 p-10 md:m-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
   )
 }
 
-export default Line
+export default Line;
